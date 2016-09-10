9674f961013456597b8ed7f0edb38d51d8404071
Taylor Widman <tpwidman@gmail.com>
2016-09-09 21:02:07 -0500

    added some spacing for readability

--- numbers.js
+++ numbers.js
@@ -1,15 +1,19 @@
 for(var i = 11; i <= 99; i += 11){
 	alert("i = " + i);
 }
+
 if( i == 99){
 i + 12;
 }
+
 for(var i = 111; i <= 999; i +=111){
 	alert("i = "+ i);
 }
+
 if( i == 999){
 i + 112;
 }
+
 for(var i = 1111; i <= 9999; i += 1111){
 	alert("i = "+ i);
 }
